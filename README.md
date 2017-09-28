5ebdb9f948bd2eb7f948246b439ab8e4893c4f29
liaolin <208927877@qq.com>
2017-09-28 17:38:08 +0800

    add demo 、 README.md

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
 # pcm_to_wav
 pcm音频数据流转wav直接播放。【PCM audio stream 】
 
+# 应用场景
+## 在线直接播放pcm音频数据。
+
+## Example
+
+```html
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="description" content="pcm player">
+		<meta charset="UTF-8">
+        <title>pcm to wav </title>
+    </head>
+    <body>
+        采样率 <input type="number" value=11025 min=0 id="sampleRate"/>
+        采样精度    <select id="sampleBits">
+        <option value="8">8bits</option>
+        <option value="16" selected>16bits</option>
+        <option value="32">32bits</option>
+        </select>
+        声道  <select id="channelCount">
+        <option value="1">单声道</option>
+        <option value="2">双声道</option>
+        </select>
+
+        //引入pcm_to_wav.js
+        <script type="text/javascript" src="pcm_to_wav.js"></script>
+        <script type="text/javascript">
+        	//直接调用即可播放pcm音频数据
+            pcm_wav('test.pcm','11025','16','1');
+        </script>
+    </body>
+</html>
+```
+
+# 根据自己的业务修改js文件，可控制pcm音频播放暂停等。
+
+# Test Demo [HTML5_Audio_Visualizer](https://loringheart.github.io/pcm_to_wav/)
+测试 test.pcm 文件有点大，网络不好的情况请稍等一下。
+ 
+# 参考资料  [HTML5_Audio_Visualizer](https://github.com/loringmore/HTML5_Audio_Visualizer)
+
+# 测试本地pcm文件 [HTML5_Audio_Visualizer](./demo/index.html)
+
 
---
+++ demo/js/html5_audio_pcm.js
@@ -0,0 +1,324 @@
+/**
+ * An audio spectrum visualizer built with HTML5 Audio API
+ * Author:Wayou
+ * License:feel free to use but keep refer pls!
+ * Feb 15, 2014
+ * For more infomation or support you can :
+ * view the project page:https://github.com/Wayou/HTML5_Audio_Visualizer/
+ * view online demo:http://wayou.github.io/HTML5_Audio_Visualizer/
+ */
+String.prototype.endWith=function(endStr){
+  var d=this.length-endStr.length;
+  return (d>=0&&this.lastIndexOf(endStr)==d)
+}
+window.onload = function() {
+    new Visualizer().ini();
+};
+var Visualizer = function() {
+    this.file = null; //the current file
+    this.fileName = null; //the current file name
+    this.audioContext = null;
+    this.source = null; //the audio source
+    this.info = document.getElementById('info').innerHTML; //this used to upgrade the UI information
+    this.infoUpdateId = null; //to sotore the setTimeout ID and clear the interval
+    this.animationId = null;
+    this.status = 0; //flag for sound is playing 1 or stopped 0
+    this.forceStop = false;
+    this.allCapsReachBottom = false;
+};
+Visualizer.prototype = {
+    ini: function() {
+        this._prepareAPI();
+        this._addEventListner();
+    },
+    _prepareAPI: function() {
+        //fix browser vender for AudioContext and requestAnimationFrame
+        window.AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || window.msAudioContext;
+        window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
+        window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || window.msCancelAnimationFrame;
+        try {
+            this.audioContext = new AudioContext();
+        } catch (e) {
+            this._updateInfo('!Your browser does not support AudioContext', false);
+            console.log(e);
+        }
+    },
+    _addEventListner: function() {
+        var that = this,
+            audioInput = document.getElementById('uploadedFile'),
+            dropContainer = document.getElementsByTagName("canvas")[0];
+        //listen the file upload
+        audioInput.onchange = function() {
+            if (that.audioContext===null) {return;};
+            
+            //the if statement fixes the file selction cancle, because the onchange will trigger even the file selection been canceled
+            if (audioInput.files.length !== 0) {
+                //only process the first file
+                that.file = audioInput.files[0];
+                that.fileName = that.file.name;
+                if (that.status === 1) {
+                    //the sound is still playing but we upload another file, so set the forceStop flag to true
+                    that.forceStop = true;
+                };
+                document.getElementById('fileWrapper').style.opacity = 1;
+                that._updateInfo('Uploading', true);
+                //once the file is ready,start the visualizer
+                that._start();
+            };
+        };
+        //listen the drag & drop
+        dropContainer.addEventListener("dragenter", function() {
+            document.getElementById('fileWrapper').style.opacity = 1;
+            that._updateInfo('Drop it on the page', true);
+        }, false);
+        dropContainer.addEventListener("dragover", function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            //set the drop mode
+            e.dataTransfer.dropEffect = 'copy';
+        }, false);
+        dropContainer.addEventListener("dragleave", function() {
+            document.getElementById('fileWrapper').style.opacity = 0.2;
+            that._updateInfo(that.info, false);
+        }, false);
+        dropContainer.addEventListener("drop", function(e) {
+            e.stopPropagation();
+            e.preventDefault();
+            if (that.audioContext===null) {return;};
+            document.getElementById('fileWrapper').style.opacity = 1;
+            that._updateInfo('Uploading', true);
+            //get the dropped file
+            that.file = e.dataTransfer.files[0];
+            if (that.status === 1) {
+                document.getElementById('fileWrapper').style.opacity = 1;
+                that.forceStop = true;
+            };
+            that.fileName = that.file.name;
+            //once the file is ready,start the visualizer
+            that._start();
+        }, false);
+    },
+    _start: function() {
+        //read and decode the file into audio array buffer 
+        var that = this,
+            file = this.file,
+            fr = new FileReader();
+        fr.onload = function(e) {
+            var fileResult = e.target.result;
+            var audioContext = that.audioContext;
+            if (audioContext === null) {
+                return;
+            };
+            that._updateInfo('Decoding the audio', true);
+
+            
+            var addWavHeader = function(samples,sampleRateTmp,sampleBits,channelCount){
+                var dataLength = samples.byteLength;
+                var buffer = new ArrayBuffer(44 + dataLength);
+                var view = new DataView(buffer);
+                function writeString(view, offset, string){
+                    for (var i = 0; i < string.length; i++){
+                        view.setUint8(offset + i, string.charCodeAt(i));
+                    }
+                }
+                
+                var offset = 0;
+                /* 资源交换文件标识符 */
+                writeString(view, offset, 'RIFF'); offset += 4;
+                /* 下个地址开始到文件尾总字节数,即文件大小-8 */
+                view.setUint32(offset, /*32*/ 36 + dataLength, true); offset += 4;
+                /* WAV文件标志 */
+                writeString(view, offset, 'WAVE'); offset += 4;
+                /* 波形格式标志 */
+                writeString(view, offset, 'fmt '); offset += 4;
+                /* 过滤字节,一般为 0x10 = 16 */
+                view.setUint32(offset, 16, true); offset += 4;
+                /* 格式类别 (PCM形式采样数据) */
+                view.setUint16(offset, 1, true); offset += 2;
+                /* 通道数 */
+                view.setUint16(offset, channelCount, true); offset += 2;
+                /* 采样率,每秒样本数,表示每个通道的播放速度 */
+                view.setUint32(offset, sampleRateTmp, true); offset += 4;
+                /* 波形数据传输率 (每秒平均字节数) 通道数×每秒数据位数×每样本数据位/8 */
+                view.setUint32(offset, sampleRateTmp * channelCount * (sampleBits / 8), true); offset +=4;
+                /* 快数据调整数 采样一次占用字节数 通道数×每样本的数据位数/8 */
+                view.setUint16(offset, channelCount * (sampleBits / 8), true); offset += 2;
+                /* 每样本数据位数 */
+                view.setUint16(offset, sampleBits, true); offset += 2;
+                /* 数据标识符 */
+                writeString(view, offset, 'data'); offset += 4;
+                /* 采样数据总数,即数据总大小-44 */
+                view.setUint32(offset, dataLength, true); offset += 4;
+                function floatTo32BitPCM(output, offset, input){
+                    input = new Int32Array(input);
+                    for (var i = 0; i < input.length; i++, offset+=4){
+                        output.setInt32(offset,input[i],true);
+                    }
+                }
+                function floatTo16BitPCM(output, offset, input){
+                    input = new Int16Array(input);
+                    for (var i = 0; i < input.length; i++, offset+=2){
+                        output.setInt16(offset,input[i],true);
+                    }
+                }
+                function floatTo8BitPCM(output, offset, input){
+                    input = new Int8Array(input);
+                    for (var i = 0; i < input.length; i++, offset++){
+                        output.setInt8(offset,input[i],true);
+                    }
+                }
+                if(sampleBits == 16){
+                    floatTo16BitPCM(view, 44, samples);
+                }else if(sampleBits == 8){
+                    floatTo8BitPCM(view, 44, samples);
+                }else{
+                    floatTo32BitPCM(view, 44, samples);
+                }
+                return view.buffer;
+            }
+            if(that.fileName.endWith(".pcm")){
+                var sampleRateTmp = document.getElementById('sampleRate').value;
+                var sampleBits = document.getElementById('sampleBits').value;
+                var channelCount = document.getElementById('channelCount').value;
+                fileResult = addWavHeader(fileResult,sampleRateTmp,sampleBits,channelCount);
+            }
+
+            audioContext.decodeAudioData(fileResult, function(buffer) {
+                that._updateInfo('Decode succussfully,start the visualizer', true);
+                that._visualize(audioContext, buffer);
+            }, function(e) {
+                that._updateInfo('!Fail to decode the file', false);
+                console.log(e);
+            });
+        };
+        fr.onerror = function(e) {
+            that._updateInfo('!Fail to read the file', false);
+            console.log(e);
+        };
+        //assign the file to the reader
+        this._updateInfo('Starting read the file', true);
+        fr.readAsArrayBuffer(file);
+    },
+    _visualize: function(audioContext, buffer) {
+        var audioBufferSouceNode = audioContext.createBufferSource(),
+            analyser = audioContext.createAnalyser(),
+            that = this;
+        //connect the source to the analyser
+        audioBufferSouceNode.connect(analyser);
+        //connect the analyser to the destination(the speaker), or we won't hear the sound
+        analyser.connect(audioContext.destination);
+        //then assign the buffer to the buffer source node
+        audioBufferSouceNode.buffer = buffer;
+        //play the source
+        if (!audioBufferSouceNode.start) {
+            audioBufferSouceNode.start = audioBufferSouceNode.noteOn //in old browsers use noteOn method
+            audioBufferSouceNode.stop = audioBufferSouceNode.noteOff //in old browsers use noteOff method
+        };
+        //stop the previous sound if any
+        if (this.animationId !== null) {
+            cancelAnimationFrame(this.animationId);
+        }
+        if (this.source !== null) {
+            this.source.stop(0);
+        }
+        audioBufferSouceNode.start(0);
+        this.status = 1;
+        this.source = audioBufferSouceNode;
+        audioBufferSouceNode.onended = function() {
+            that._audioEnd(that);
+        };
+        this._updateInfo('Playing ' + this.fileName, false);
+        this.info = 'Playing ' + this.fileName;
+        document.getElementById('fileWrapper').style.opacity = 0.2;
+        this._drawSpectrum(analyser);
+    },
+    _drawSpectrum: function(analyser) {
+        var that = this,
+            canvas = document.getElementById('canvas'),
+            cwidth = canvas.width,
+            cheight = canvas.height - 2,
+            meterWidth = 10, //width of the meters in the spectrum
+            gap = 2, //gap between meters
+            capHeight = 2,
+            capStyle = '#fff',
+            meterNum = 800 / (10 + 2), //count of the meters
+            capYPositionArray = []; ////store the vertical position of hte caps for the preivous frame
+        ctx = canvas.getContext('2d'),
+        gradient = ctx.createLinearGradient(0, 0, 0, 300);
+        gradient.addColorStop(1, '#0f0');
+        gradient.addColorStop(0.5, '#ff0');
+        gradient.addColorStop(0, '#f00');
+        var drawMeter = function() {
+            var array = new Uint8Array(analyser.frequencyBinCount);
+            analyser.getByteFrequencyData(array);
+            if (that.status === 0) {
+                //fix when some sounds end the value still not back to zero
+                for (var i = array.length - 1; i >= 0; i--) {
+                    array[i] = 0;
+                };
+                allCapsReachBottom = true;
+                for (var i = capYPositionArray.length - 1; i >= 0; i--) {
+                    allCapsReachBottom = allCapsReachBottom && (capYPositionArray[i] === 0);
+                };
+                if (allCapsReachBottom) {
+                    cancelAnimationFrame(that.animationId); //since the sound is stoped and animation finished, stop the requestAnimation to prevent potential memory leak,THIS IS VERY IMPORTANT!
+                    return;
+                };
+            };
+            var step = Math.round(array.length / meterNum); //sample limited data from the total array
+            ctx.clearRect(0, 0, cwidth, cheight);
+            for (var i = 0; i < meterNum; i++) {
+                var value = array[i * step];
+                if (capYPositionArray.length < Math.round(meterNum)) {
+                    capYPositionArray.push(value);
+                };
+                ctx.fillStyle = capStyle;
+                //draw the cap, with transition effect
+                if (value < capYPositionArray[i]) {
+                    ctx.fillRect(i * 12, cheight - (--capYPositionArray[i]), meterWidth, capHeight);
+                } else {
+                    ctx.fillRect(i * 12, cheight - value, meterWidth, capHeight);
+                    capYPositionArray[i] = value;
+                };
+                ctx.fillStyle = gradient; //set the filllStyle to gradient for a better look
+                ctx.fillRect(i * 12 /*meterWidth+gap*/ , cheight - value + capHeight, meterWidth, cheight); //the meter
+            }
+            that.animationId = requestAnimationFrame(drawMeter);
+        }
+        this.animationId = requestAnimationFrame(drawMeter);
+    },
+    _audioEnd: function(instance) {
+        if (this.forceStop) {
+            this.forceStop = false;
+            this.status = 1;
+            return;
+        };
+        this.status = 0;
+        var text = 'HTML5 Audio API showcase | An Audio Viusalizer';
+        document.getElementById('fileWrapper').style.opacity = 1;
+        document.getElementById('info').innerHTML = text;
+        instance.info = text;
+        document.getElementById('uploadedFile').value = '';
+    },
+    _updateInfo: function(text, processing) {
+        var infoBar = document.getElementById('info'),
+            dots = '...',
+            i = 0,
+            that = this;
+        infoBar.innerHTML = text + dots.substring(0, i++);
+        if (this.infoUpdateId !== null) {
+            clearTimeout(this.infoUpdateId);
+        };
+        if (processing) {
+            //animate dots at the end of the info text
+            var animateDot = function() {
+                if (i > 3) {
+                    i = 0
+                };
+                infoBar.innerHTML = text + dots.substring(0, i++);
+                that.infoUpdateId = setTimeout(animateDot, 250);
+            }
+            this.infoUpdateId = setTimeout(animateDot, 250);
+        };
+    }
+}
